01ff524b61a9830e75de285c8e15ea1562ddb633
Andrew Hipp <andrew.hipp@gmail.com>
2018-04-16 10:49:40 -0500

    Updating phylogenetic visualization

--- SCRIPTS/04.phylo.v1.R
+++ SCRIPTS/04.phylo.v1.R
@@ -12,9 +12,40 @@ ndvi.mat.mean <- sapply(ndvi.mat.spl, function(x) {
   apply(x, 2, function(y) mean(as.numeric(y), rm.na = T))
   }) %>%
 t %>%
-scale %>%
 as.data.frame
 
-p <- ggtree(tr.prairie, layout = 'circular')
-p <- gheatmap(p, data = ndvi.mat.mean[c('biomass.monocultures', 'ndvi')])
+ndvi.mat.small <- ndvi.mat.mean[c('biomass.monocultures', 'ndvi')]
+names(ndvi.mat.small) <- c('Biomass', 'NDVI')
+#for(i in 1:2) ndvi.mat.small[[i]] <- log(ndvi.mat.small[[i]])
+#for(i in 1:2) ndvi.mat.small[[i]] <- sqrt(ndvi.mat.small[[i]])
+## remove all NAs and -INF and prune tree
+problem.rows <- which(apply(ndvi.mat.small, 1, function(x) any(c(-Inf, NaN) %in% x)))
+if(length(problem.rows) > 0) ndvi.mat.small <- ndvi.mat.small[-problem.rows, ]
+
+ndvi.mat.small <- apply(ndvi.mat.small, 2, function(x) (x - min(x)) / max(x - min(x)))
+problem.rows <- which(apply(ndvi.mat.small, 1, sum, na.rm = T) < 0.10)
+if(length(problem.rows) > 0) ndvi.mat.small <- ndvi.mat.small[-problem.rows, ]
+
+tr.prairie.biomassPlot <- drop.tip(tr.prairie, names(problem.rows))
+tr.prairie.biomassPlot$node.label[tr.prairie.biomassPlot$node.label %in% c('', 'NA')] <- NA
+tr.prairie.biomassPlot$node.label <-
+  c(rep(NA, length(tr.prairie.biomassPlot$tip.label)), tr.prairie.biomassPlot$node.label)
+
+## rescale to 0:1
+
+pdf('../OUT/prairie.biomass.pdf')
+p <- ggtree(tr.prairie.biomassPlot
+#            layout = 'fan',
+#            open.angle = 15
+          )
+p <- p + geom_label(aes(x = branch), label = tr.prairie.biomassPlot$node.label, size = 2)
+p <- gheatmap(p, data = ndvi.mat.small,
+              low = 'white', high = 'black',
+              colnames_angle = 315,
+              font.size = 2,
+              width = 0.1,
+              hjust = 0,
+              )
+p <- p + theme(legend.position = c(0.05,0.9))
 print(p)
+dev.off()
