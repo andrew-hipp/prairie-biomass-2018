9e0e476501e94d47030408359d805636c56f135e
andrew-hipp <andrew.hipp@gmail.com>
2018-07-12 11:07:33 -0500

    Adding problem spp from 2017

---
+++ SCRIPTS/00b.problemSpp.2017.R
@@ -0,0 +1,30 @@
+## problem spp identified in 2017 study of cover
+## ahipp 2018-07-12
+
+spp.spring <- sapply(c('Maianthemum',
+                      'Polygonatum',
+                      'Dodecatheon',
+                      'Allium',
+                      'Camassia'),
+                    function(x) grep(x, tr.prairie$tip.label, value = T)) %>%
+              unlist
+
+
+spp.didNotEstablishWell = c('Castilleja_coccinea',
+                       'Asclepias_hirtella',
+                       'Asclepias_syriaca',
+                       'Dalea_purpurea',
+                       "Helianthus_strumosus",
+                       'Juncus_interior',
+                       'Juncus_tenuis',
+                       'Verbena_stricta',
+                       'Asclepias_sullivantii',
+                       'Baptisia_bracteata',
+                       'Ceanothus_americanus',
+                       'Dalea_candida',
+                       'Senna_hebecarpa',
+                       'Hypericum_punctatum',
+                       'Lilium_michiganense',
+                       'Symphyotrichum_sericeum')
+
+spp.prob.2017 <- unique(c(spp.spring, spp.didNotEstablishWell))
