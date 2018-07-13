f55baab0b2fbee971fe7693ee772bc2cfa18a3af
andrew-hipp <andrew.hipp@gmail.com>
2018-07-13 12:26:47 -0500

    Ordination cleaned up, using corrected data and names

--- SCRIPTS/05.traitNDVI.biomass.R
+++ SCRIPTS/05.traitNDVI.biomass.R
@@ -1,6 +1,7 @@
 ## ordination of biomass and NDVI on traits
 ## ah, 2018-07-12 update:
 ##  clean up legend on biomass - ndvi correlation
+##  removing problem spp.2017
 
 library(vegan)
 library(FD)
@@ -10,48 +11,34 @@ library(ggrepel)
 library(magrittr)
 library(latex2exp)
 
-dat.fams <- read.csv('../DATA/families.csv', row.names = 1, as.is = T)
-dat.traits <- read.csv('../DATA/ImputedMiceTraits.2016-01-06.csv',
-                       as.is = F, row.names = 1)
-
-dat.traits.use <- c("seedMass", "LDMC", "SLA", "LNC", "LCC", "LPC", "SDMC", "circularity",
-                       "vegetativeHeight", "leafLength", "leafThickness", "leafWidth",
-                       "petioleLength", "Lifeform", "photosyntheticPathway", "rhizomes",
-                       "habitClean", "nFixer",
-                       "wis_IL", "genome_Use")
-dat.traits.continuous <- c("seedMass", "LDMC", "SLA", "LNC", "LCC", "LPC", "SDMC", "circularity",
-                                              "vegetativeHeight", "leafLength", "leafThickness", "leafWidth",
-                                              "petioleLength","genome_Use")
-dat.traits.scaled <- dat.traits[row.names(ndvi.mat.small), dat.traits.use]
-for(i in dat.traits.continuous) dat.traits.scaled[[i]] <- as.numeric(scale(dat.traits.scaled[[i]]))
-dat.fams$Family.8 <- dat.fams$family
-dat.fams$Family.8[!dat.fams$family %in% names(tail(sort(table(dat.fams$family)), 7))] <- "Other"
-dat.traits.mds <- monoMDS(gowdis(dat.traits.scaled))
+labels.mapping <- c('Stem dry matter content', 'Leaf circularity', 'Vegetative height',
+                     'Leaf dry matter content', 'Leaf C content', 'Leaf length', 'Leaf thickness',
+                     'Biomass', 'NDVI', 'GNDVI')
+dat.traits.mds <- monoMDS(gowdis(all.prairie.ordi[, labels.traits.use]))
 dat.mds <- as.data.frame(dat.traits.mds$points)
-# dat.mds$Family <- dat.fams[row.names(dat.mds), 'family']
-dat.mds$Family <- factor(dat.fams[row.names(dat.mds), 'Family.8'],
-                          levels = c("Asclepiadaceae", "Asteraceae", "Cyperaceae",
+
+mds.index <- gsub(" ", "_", row.names(dat.mds))
+dat.mds$Family <- factor(dat.fams[mds.index, 'Family.8'],
+                          levels = c("Apocynaceae", "Asteraceae", "Cyperaceae",
                           "Fabaceae", "Lamiaceae", "Poaceae", "Rosaceae",
                           "Other"))
-dat.mds$Biomass <- ndvi.mat.mean[row.names(dat.mds), 'biomass.all']
+dat.mds$Biomass <- all.prairie.ordi[row.names(dat.mds), 'Biomass']
 
-dat.traits.mapping <- data.frame(dat.traits.scaled[c('SDMC', 'circularity', 'vegetativeHeight',
-                                'LDMC', 'LCC', 'leafLength', 'leafThickness')],
-                                Biomass = ndvi.mat.mean[row.names(dat.mds), 'biomass.all'],
-                                NDVI = ndvi.mat.mean[row.names(dat.mds), 'ndvi']
-                                )
+dat.traits.mapping <- all.prairie.ordi[labels.mapping]
+
 dat.traits.env <- envfit(dat.traits.mds, dat.traits.mapping)
 dat.traits.arrows <- as.data.frame(dat.traits.env$vector$arrows*sqrt(dat.traits.env$vectors$r) * 3)
 dat.traits.arrows$Trait <- row.names(dat.traits.arrows)
 dat.traits.arrows$Mapping_variable <-
-  ifelse(dat.traits.arrows$Trait %in% c('Biomass', 'NDVI'), 'Productivity response', 'Plant trait') %>%
+  ifelse(dat.traits.arrows$Trait %in% c('Biomass', 'NDVI', 'GNDVI'), 'Productivity response', 'Plant trait') %>%
   factor(levels = c('Productivity response', 'Plant trait'))
 p.mds <- ggplot(dat.mds, aes(x = MDS1, y = MDS2))
 p.mds <- p.mds + geom_point(aes(size = Biomass, color = Family))
-p.mds <- p.mds + scale_color_brewer(type = 'qual', palette = 1)
+p.mds <- p.mds + scale_color_brewer("Plant family", type = 'qual', palette = 1)
 #p.mds <- p.mds <- scale_color_brewer("Top 7 families")
-# p.mds <- p.mds + geom_label_repel(label = row.names(dat.mds),point.padding = 1)
-#p.mds <- p.mds + theme(legend.position = c(0.9,0.1))
+p.mds <- p.mds + theme(legend.position = 'right',
+                        legend.key = element_blank()
+                        )
 p.mds <- p.mds + geom_segment(data = dat.traits.arrows,
                               aes(x = 0, xend = MDS1, y = 0, yend = MDS2, linetype = Mapping_variable),
                               arrow = arrow(length = unit(0.5, 'cm')), color = 'black'
@@ -61,11 +48,12 @@ p.mds <- p.mds + geom_label_repel(data = dat.traits.arrows,
                                 point.padding = 0.5, segment.size = 0)
 #p.mds <- p.mds + coord_fixed()
 
-pdf('../OUT/trait.ordination.withBiomass.pdf',12, 8)
+pdf('../OUT/FIGURE.trait.ordination.withBiomass.pdf',12, 8)
 print(p.mds)
 dev.off()
 
 #plot(dat.traits.env)
+stop('debugging stop')
 
 if(!exists("dat.traits.lambda")) {
   dat.traits.lambda <- fitContinuous(tr.prairie.phylosig,
@@ -95,6 +83,6 @@ p <- p + theme(
                )
 p <- p + scale_radius(TeX("Pagel's $\\lambda$"))
 
-pdf('../OUT/biomass.ndvi.correlation.regression.pdf', 8, 8)
+pdf('../OUT/FIGURE.biomass.ndvi.correlation.regression.pdf', 8, 8)
 print(p)
 dev.off()
