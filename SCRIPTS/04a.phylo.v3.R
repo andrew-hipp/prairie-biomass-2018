8c284160dd088cfb1e974ca5e662de1f7183b331
andrew-hipp <andrew.hipp@gmail.com>
2018-07-13 12:25:42 -0500

    Phylogeny cleaned up, renamed for script sequence

--- SCRIPTS/04a.phylo.v3.R
+++ SCRIPTS/04a.phylo.v3.R
@@ -16,60 +16,12 @@ library(tidyverse)
 
 relabelBranches = TRUE
 
-source('../SCRIPTS/00b.problemSpp.2017.R')
 source('../SCRIPTS/999.gheatmap.mod.R') # to suppress legend selectively
-spp.prob.2017 <- gsub('_', ' ', spp.prob.2017, fixed = T)
 
 tr.prairie.biomassPlot <- tr.prairie
 tr.prairie.biomassPlot$tip.label[which(tr.prairie.biomassPlot$tip.label == "Symphyotrichum_novaeangliae")] <-
   "Symphyotrichum_novae-angliae"
 tr.prairie.biomassPlot$tip.label <- gsub('_', ' ', tr.prairie.biomassPlot$tip.label, fixed = T)
-
-##NDVI
-
-all.prairie.spl <- split(all.prairie, all.prairie$monoTreeName) ##changed ndvi.mat to all.prairie to complie with 01.compileData.R
-all.prairie.mean <- sapply(all.prairie.spl, function(x) {
-  apply(x, 2, function(y) mean(as.numeric(y), rm.na = T))
-  }) %>%
-  t %>%
-  as.data.frame
-
-all.prairie.small <- all.prairie.mean[c('biomass.monocultures',
-                                        'pNDVIvalues',
-                                        'pGNDVIvalues',
-                                        'pGDVI2values',
-                                        'pNIRvalues',
-                                        'pREGvalues',
-                                        'pREDvalues',
-                                        'pGREvalues')]
-
-names(all.prairie.small) <- c('Biomass',
-                              'NDVI',
-                              'GNDVI',
-                              'GDVI2',
-                              'pNIR',
-                              'pREG',
-                              'pRED',
-                              'pGRE')
-
-row.names(all.prairie.small) <- gsub('_', ' ', row.names(all.prairie.small), fixed = T)
-
-message('CHECKING FOR 2017 PROBLEM SPECIES')
-if(any(!spp.prob.2017 %in% row.names(all.prairie.small))) {
-  message("... the following problem spp were not found:")
-  print(setdiff(spp.prob.2017, row.names(all.prairie.small)))
-  } else
-  message("... and all are excluded from all.prairie.small")
-
-all.prairie.small <- all.prairie.small[which(!row.names(all.prairie.small) %in% spp.prob.2017), ]
-
-## remove all NAs and -INF and prune tree
-problem.rows <- which(apply(all.prairie.small, 1, function(x) any(c(-Inf, NaN) %in% x)))
-if(length(problem.rows) > 0) all.prairie.small <- all.prairie.small[-problem.rows, ]
-
-all.prairie.small <- apply(all.prairie.small, 2, function(x) (x - min(x)) / max(x - min(x))) %>%
-  as.data.frame
-if(length(problem.rows) > 0) all.prairie.small <- all.prairie.small[-problem.rows, ]
 
 tr.prairie.biomassPlot <- drop.tip(tr.prairie.biomassPlot, which(!tr.prairie.biomassPlot$tip.label %in% row.names(all.prairie.small))) %>%
   multi2di
@@ -85,7 +37,10 @@ tr.prairie.biomassPlot <- groupClade(tr.prairie.biomassPlot,
                                      c(tr.mrca['Helianthus occidentalis', 'Ratibida pinnata'],
                                    tr.mrca['Lespedeza capitata', 'Desmanthus illinoensis'],
                                    tr.mrca['Bromus kalmii', 'Bouteloua curtipendula'],
-                                   tr.mrca['Pycnanthemum virginianum', 'Physostegia virginiana']
+                                   tr.mrca['Pycnanthemum virginianum', 'Physostegia virginiana'],
+                                   tr.mrca['Carex bicknellii', 'Carex blanda'],
+                                   tr.mrca['Asclepias tuberosa', 'Asclepias verticillata'],
+                                   tr.mrca['Rosa blanda', 'Spiraea alba']
                                    )
                                  )
 if(relabelBranches) {
@@ -93,7 +48,10 @@ if(relabelBranches) {
                                       'Asteraceae - Sunflower Family',
                                       'Fabaceae - Bean Family',
                                       'Poaceae - Grass Family',
-                                      'Lamiaceae - Mint Family')[attr(tr.prairie.biomassPlot, 'group')]
+                                      'Lamiaceae - Mint Family',
+                                      'Cyperaceae - Sedge Family',
+                                      'Apocynaceae - Milkweed Family',
+                                      'Rosaceae - Rose Family')[attr(tr.prairie.biomassPlot, 'group')]
                                       )
                                     }
 
@@ -101,13 +59,15 @@ pdf('../OUT/FIGURE.prairie.biomass.allSpectra.withColors.pdf')
 p <- ggtree(tr.prairie.biomassPlot,
             aes(color=group)
           )
-p <- p + scale_color_manual("Major plant families",
-                            values = c('black',
-                                        'orange',
-                                        'maroon1',
-                                        'lightgreen',
-                                        'blue')
-                                        )
+#p <- p + scale_color_manual("Major plant families",
+#                            values = c('black',
+#                                        'orange',
+#                                        'maroon1',
+#                                        'lightgreen',
+#                                        'blue',
+#                                        '')
+#                                        )
+p <- p + scale_color_brewer("Plant family", type = 'qual', palette = 1)
 #p <- p + geom_label(aes(x = branch), label = tr.prairie.biomassPlot$node.label, size = 2)
 p <- gheatmap(p, data = all.prairie.small,
               low = 'white', high = 'black',
