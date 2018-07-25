8f31483dba23ec07dde122c6b2c30614121c5d6a
lanescher <37148093+lanescher@users.noreply.github.com>
2018-07-25 13:20:50 -0400

    added script to store deleted lines from 106.making.graphs

--- SCRIPTS/106.making.graphs.r
+++ SCRIPTS/106.making.graphs.r
@@ -15,48 +15,9 @@ prairie.use <- all.prairie
 
 ################ regression
 
-predictor = c("pNDVIvalues", "pGDVI2values", "pGNDVIvalues")
-response = c("biomass.all", "coverTotal")
-predictornames = c("NDVI", "GDVI2", "GNDVI")
-responsenames = c("Biomass", "Cover")
-
-
-i <- 1
-j <- 1
-
-for (i in 1:length(predictor)) {
-  jpeg(paste0("plot_", predictornames[i], "_", responsenames[j], ".jpg"), width = 480, height = 480)
-  ggplot(data = prairie.use, 
-       aes(x = prairie.use[[predictor[i]]], y = prairie.use[[response[j]]])) +
-    geom_point(aes(color = factor(prairie.use$Plot.category))) +
-    geom_smooth(method = "lm", aes(color = factor(prairie.use$Plot.category))) +
-    labs(title = paste0(predictornames[i], " vs ", responsenames[j]), 
-       x = predictornames[i], y = responsenames[j]) +
-    scale_colour_manual(values = c("#4682B4", "#B4464B"),
-                    name = "Plot type",
-                    breaks = c("Monoculture", "Treatment"))
-  dev.off()
-  i <- i + 1
-}
-
-
-
-i <- 1
-j <- 2
-
-jpeg(paste0("plot_", predictornames[i], "_", responsenames[j], ".jpg"), width = 480, height = 480)
-ggplot(data = prairie.use, 
-       aes(x = prairie.use[[predictor[i]]], y = prairie.use[[response[j]]])) +
-  geom_point(aes(color = factor(prairie.use$Plot.category))) +
-  geom_smooth(method = "lm", aes(color = factor(prairie.use$Plot.category))) +
-  labs(title = paste0(predictornames[i], " vs ", responsenames[j]), 
-       x = predictornames[i], y = responsenames[j]) +
-  scale_colour_manual(values = c("#4682B4", "#B4464B"),
-                      name = "Plot type",
-                      breaks = c("Monoculture", "Treatment"))
-dev.off()
 
 # combined
+# NDVI biomass regression
 NBR <- ggplot(data = prairie.use, 
        aes(x = prairie.use$pNDVIvalues, y = prairie.use$biomass.all)) +
   geom_point(aes(color = factor(prairie.use$Plot.category))) +
@@ -68,6 +29,7 @@ NBR <- ggplot(data = prairie.use,
                       guide = FALSE) +
   theme_classic()
 
+# NDVI cover regression
 NCR <- ggplot(data = prairie.use, 
               aes(x = prairie.use$pNDVIvalues, y = prairie.use$coverTotal)) +
   geom_point(aes(color = factor(prairie.use$Plot.category))) +
@@ -100,8 +62,9 @@ lighten <- function(color, factor=1.4){
   col
 }
 
-# biomass
 
+
+# biomass
 MB <- ggplot(data = prairie.use,
              aes(x = prairie.use$Plot.category, y = prairie.use$biomass.all)) +
   geom_boxplot(fill = c("goldenrod2", "cornflowerblue")) +
