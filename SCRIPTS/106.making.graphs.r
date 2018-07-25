04126da4ec131f71803dbc0cd9a39edb0bb2d7f5
lanescher <37148093+lanescher@users.noreply.github.com>
2018-07-25 13:45:18 -0400

    figures use correct tmt plots for biomass

--- SCRIPTS/106.making.graphs.r
+++ SCRIPTS/106.making.graphs.r
@@ -4,13 +4,11 @@ library(ggplot2)
 library(colortools)
 library(ggpubr)
 
-all.prairie <- read.csv("DATA/all.prairie.with.VI.values.csv")
 
-
-prairie.use <- all.prairie[which(all.prairie$Plot.category == "Monoculture" |
+prairie.use.biomass <- all.prairie[which(all.prairie$Plot.category == "Monoculture" |
                                    all.prairie$TMT.use == 1),]
 
-prairie.use <- all.prairie
+prairie.use.other <- all.prairie
 
 
 ################ regression
@@ -18,10 +16,10 @@ prairie.use <- all.prairie
 
 # combined
 # NDVI biomass regression
-NBR <- ggplot(data = prairie.use, 
-       aes(x = prairie.use$pNDVIvalues, y = prairie.use$biomass.all)) +
-  geom_point(aes(color = factor(prairie.use$Plot.category))) +
-  geom_smooth(method = "lm", aes(color = factor(prairie.use$Plot.category))) +
+NBR <- ggplot(data = prairie.use.biomass, 
+       aes(x = prairie.use.biomass$pNDVIvalues, y = prairie.use.biomass$biomass.all)) +
+  geom_point(aes(color = factor(prairie.use.biomass$Plot.category))) +
+  geom_smooth(method = "lm", aes(color = factor(prairie.use.biomass$Plot.category))) +
   labs(x = "NDVI", y = "biomass") +
   scale_colour_manual(values = c("goldenrod2", "cornflowerblue"),
                       name = "Plot type",
@@ -30,10 +28,10 @@ NBR <- ggplot(data = prairie.use,
   theme_classic()
 
 # NDVI cover regression
-NCR <- ggplot(data = prairie.use, 
-              aes(x = prairie.use$pNDVIvalues, y = prairie.use$coverTotal)) +
-  geom_point(aes(color = factor(prairie.use$Plot.category))) +
-  geom_smooth(method = "lm", aes(color = factor(prairie.use$Plot.category))) +
+NCR <- ggplot(data = prairie.use.other, 
+              aes(x = prairie.use.other$pNDVIvalues, y = prairie.use.other$coverTotal)) +
+  geom_point(aes(color = factor(prairie.use.other$Plot.category))) +
+  geom_smooth(method = "lm", aes(color = factor(prairie.use.other$Plot.category))) +
   labs(x = "NDVI", y = "percent cover") +
   scale_colour_manual(values = c("goldenrod2", "cornflowerblue"),
                       name = "Plot type",
@@ -65,32 +63,32 @@ lighten <- function(color, factor=1.4){
 
 
 # biomass
-MB <- ggplot(data = prairie.use,
-             aes(x = prairie.use$Plot.category, y = prairie.use$biomass.all)) +
+MB <- ggplot(data = prairie.use.biomass,
+             aes(x = prairie.use.biomass$Plot.category, y = prairie.use.biomass$biomass.all)) +
   geom_boxplot(fill = c("goldenrod2", "cornflowerblue")) +
   scale_x_discrete(limits = c("Monoculture", "Treatment"), labels = c("Monoculture", "Treatment")) +
   theme_classic() +
   labs(x = "plot type", y = "biomass") +
   ylim(c(0, 4000))
 
-TB <- ggplot(data = prairie.use,
-       aes(x = prairie.use$trait.div, y = prairie.use$biomass.all)) +
+TB <- ggplot(data = prairie.use.biomass,
+       aes(x = prairie.use.biomass$trait.div, y = prairie.use.biomass$biomass.all)) +
   geom_boxplot(fill = c(lighten("cornflowerblue"), darken("cornflowerblue"))) +
   scale_x_discrete(limits = c("L", "H"), labels = c("Low", "High")) +
   theme_classic() +
   labs(x = "trait diversity", y = "biomass") +
   ylim(c(0, 4000))
 
-PB <- ggplot(data = prairie.use,
-       aes(x = prairie.use$phy.div, y = prairie.use$biomass.all)) +
+PB <- ggplot(data = prairie.use.biomass,
+       aes(x = prairie.use.biomass$phy.div, y = prairie.use.biomass$biomass.all)) +
   geom_boxplot(fill = c(lighten("cornflowerblue"), "cornflowerblue", darken("cornflowerblue"))) +
   scale_x_discrete(limits = c("L", "M", "H"), labels = c("Low", "Medium", "High")) +
   theme_classic() +
   labs(x = "phylogenetic diversity", y = "biomass") +
   ylim(c(0, 4000))
 
-BB <- ggplot(data = prairie.use,
-       aes(x = prairie.use$block, y = prairie.use$biomass.all)) +
+BB <- ggplot(data = prairie.use.biomass,
+       aes(x = prairie.use.biomass$block, y = prairie.use.biomass$biomass.all)) +
   geom_boxplot(fill = "darkolivegreen4") +
   scale_x_discrete(limits = c("A", "B", "C", "D", "E", "F")) +
   theme_classic() +
@@ -99,32 +97,32 @@ BB <- ggplot(data = prairie.use,
 
 # NDVI
 
-TN <- ggplot(data = prairie.use,
-       aes(x = prairie.use$trait.div, y = prairie.use$pNDVIvalues)) +
+TN <- ggplot(data = prairie.use.other,
+       aes(x = prairie.use.other$trait.div, y = prairie.use.other$pNDVIvalues)) +
   geom_boxplot(fill = c(lighten("cornflowerblue"), darken("cornflowerblue"))) +
   scale_x_discrete(limits = c("L", "H"), labels = c("Low", "High")) +
   theme_classic() +
   labs(x = "trait diversity", y = "NDVI") +
   ylim(c(0.3, 0.9))
 
-PN <- ggplot(data = prairie.use,
-       aes(x = prairie.use$phy.div, y = prairie.use$pNDVIvalues)) +
+PN <- ggplot(data = prairie.use.other,
+       aes(x = prairie.use.other$phy.div, y = prairie.use.other$pNDVIvalues)) +
   geom_boxplot(fill = c(lighten("cornflowerblue"), "cornflowerblue", darken("cornflowerblue"))) +
   scale_x_discrete(limits = c("L", "M", "H"), labels = c("Low", "Medium", "High")) +
   theme_classic() +
   labs(x = "phylogenetic diversity", y = "NDVI") +
   ylim(c(0.3, 0.9))
 
-BN <- ggplot(data = prairie.use,
-             aes(x = prairie.use$block, y = prairie.use$pNDVIvalues)) +
+BN <- ggplot(data = prairie.use.other,
+             aes(x = prairie.use.other$block, y = prairie.use.other$pNDVIvalues)) +
   geom_boxplot(fill = "darkolivegreen4") +
   scale_x_discrete(limits = c("A", "B", "C", "D", "E", "F")) +
   theme_classic() +
   labs(x = "block", y = "NDVI") +
   ylim(c(0.3, 0.9))
 
-MN <- ggplot(data = prairie.use,
-       aes(x = prairie.use$Plot.category, y = prairie.use$pNDVIvalues)) +
+MN <- ggplot(data = prairie.use.other,
+       aes(x = prairie.use.other$Plot.category, y = prairie.use.other$pNDVIvalues)) +
   geom_boxplot(fill = c("goldenrod2", "cornflowerblue")) +
   scale_x_discrete(limits = c("Monoculture", "Treatment"), labels = c("Monoculture", "Treatment")) +
   theme_classic() +
@@ -132,32 +130,32 @@ MN <- ggplot(data = prairie.use,
   ylim(c(0.3, 0.9))
 
 # Cover
-TC <- ggplot(data = prairie.use,
-             aes(x = prairie.use$trait.div, y = prairie.use$coverTotal)) +
+TC <- ggplot(data = prairie.use.other,
+             aes(x = prairie.use.other$trait.div, y = prairie.use.other$coverTotal)) +
   geom_boxplot(fill = c(lighten("cornflowerblue"), darken("cornflowerblue"))) +
   scale_x_discrete(limits = c("L", "H"), labels = c("Low", "High")) +
   theme_classic() +
   labs(x = "trait diversity", y = "cover") +
   ylim(c(0, 100))
 
-PC <- ggplot(data = prairie.use,
-             aes(x = prairie.use$phy.div, y = prairie.use$coverTotal)) +
+PC <- ggplot(data = prairie.use.other,
+             aes(x = prairie.use.other$phy.div, y = prairie.use.other$coverTotal)) +
   geom_boxplot(fill = c(lighten("cornflowerblue"), "cornflowerblue", darken("cornflowerblue"))) +
   scale_x_discrete(limits = c("L", "M", "H"), labels = c("Low", "Medium", "High")) +
   theme_classic() +
   labs(x = "phylogenetic diversity", y = "cover") +
   ylim(c(0, 100))
 
-BC <- ggplot(data = prairie.use,
-             aes(x = prairie.use$block, y = prairie.use$coverTotal)) +
+BC <- ggplot(data = prairie.use.other,
+             aes(x = prairie.use.other$block, y = prairie.use.other$coverTotal)) +
   geom_boxplot(fill = "darkolivegreen4") +
   scale_x_discrete(limits = c("A", "B", "C", "D", "E", "F")) +
   theme_classic() +
   labs(x = "block", y = "cover") +
   ylim(c(0, 100))
 
-MC <- ggplot(data = prairie.use,
-             aes(x = prairie.use$Plot.category, y = prairie.use$coverTotal)) +
+MC <- ggplot(data = prairie.use.other,
+             aes(x = prairie.use.other$Plot.category, y = prairie.use.other$coverTotal)) +
   geom_boxplot(fill = c("goldenrod2", "cornflowerblue")) +
   scale_x_discrete(limits = c("Monoculture", "Treatment"), labels = c("Monoculture", "Treatment")) +
   theme_classic() +
@@ -165,7 +163,7 @@ MC <- ggplot(data = prairie.use,
   ylim(c(0, 100))
 
 
-jpeg("OUT/boxplots.jpg", width = 1100, height = 480)
+jpeg("OUT/FIGURE.boxplots.jpg", width = 1100, height = 480)
 ggarrange(MB, TB, PB, BB, 
           MC, TC, PC, BC,
           MN, TN, PN, BN, 
