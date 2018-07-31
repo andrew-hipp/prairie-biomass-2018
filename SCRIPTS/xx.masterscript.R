ddc252a1c42bc1d8aaf8ed76592f28fce1b527bc
Andrew Hipp <andrew.hipp@gmail.com>
2018-07-31 09:09:47 -0500

    Fixing case-sensitive issues on .R files and calls thereto

--- SCRIPTS/xx.masterscript.R
+++ SCRIPTS/xx.masterscript.R
@@ -1,19 +1,19 @@
 # master script that sources everything
 
 # convert .txt file with plot coordinates to list of polygons
-source("../SCRIPTS/01.makeplotpolygons.r")
+source("../SCRIPTS/01.makeplotpolygons.R")
 
 # compile data - ISSUE this script sources another file
-source("../SCRIPTS/00a.compileData.r")
+source("../SCRIPTS/00a.compileData.R")
 
 # compile more data
-source("../SCRIPTS/00b.readData.biomassPlotsTree-v3.r")
+source("../SCRIPTS/00b.readData.biomassPlotsTree-v3.R")
 
 # compile more data
-source("../SCRIPTS/00c.problemSpp.2017.r")
+source("../SCRIPTS/00c.problemSpp.2017.R")
 
 # define VI functions that will be used to calculate VI values for each plot
-source("../SCRIPTS/102.define.VI.functions.r")
+source("../SCRIPTS/102.define.VI.functions.R")
 
 # calculate VI values for each plot - this step takes a while, only run when necessary
 # source("../SCRIPTS/103.calculate.VI.things.r")
@@ -24,31 +24,31 @@ VI <- read.csv("../DATA/VIdata.csv")
 source("../SCRIPTS/107.add.VIdata.to.all.prairie")
 
 # ISSUE not sure what this script does
-source("../SCRIPTS/03.sensitiveEnvir.r")
+source("../SCRIPTS/03.sensitiveEnvir.R")
 
-### at this point, all the data has been read in and formatted 
-### into one df called all.prairie 
+### at this point, all the data has been read in and formatted
+### into one df called all.prairie
 
 # ISSUE this script sources 00b again
-source("../SCRIPTS/04a.phylo.v3.r")
+source("../SCRIPTS/04a.phylo.v3.R")
 
 # determines phylogenetic signal - ISSUE add cover?
-source("../SCRIPTS/04b.phyloSignal.r")
+source("../SCRIPTS/04b.phyloSignal.R")
 
 # trait ordination with biomass and biomass NDVI correlation
-source("../SCRIPTS/05.traitNDVI.biomass.r")
+source("../SCRIPTS/05.traitNDVI.biomass.R")
 
 # ISSUE not sure what this does
-source("../SCRIPTS/07.biomass&VI.model.ahor&block.r")
+source("../SCRIPTS/07.biomass&VI.model.ahor&block.R")
 
 # make regression summary tables for predicting biomass
-source("../SCRIPTS/104c.make.regression.summary.table.biomass.r")
+source("../SCRIPTS/104c.make.regression.summary.table.biomass.R")
 
 # make regression summary tables for predicting cover
-source("../SCRIPTS/105c.make.regrssion.summary.table.cover.r")
+source("../SCRIPTS/105c.make.regrssion.summary.table.cover.R")
 
 # make figures: NDVI/biomass and NDVI/cover regressions, summary boxplots
-source("../SCRIPTS/106.making.graphs.r")
+source("../SCRIPTS/106.making.graphs.R")
 
 # ISSUE not sure what this does
-source("../SCRIPTS/999.gheatmap.mod.r")
+source("../SCRIPTS/999.gheatmap.mod.R")
