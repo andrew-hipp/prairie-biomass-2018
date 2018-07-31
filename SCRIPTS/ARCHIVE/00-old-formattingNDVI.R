429fd91c9ecc2469af81f88582935e1f11b4e9a8
Andrew Hipp <andrew.hipp@gmail.com>
2018-07-31 09:03:24 -0500

    Moving formatting NDVI headers to archive I think these headers aren't used anywhere any longer

---
+++ SCRIPTS/ARCHIVE/00-old-formattingNDVI.R
@@ -0,0 +1,9 @@
+dat.headers.plugs <- c("plot", "type", "sp", "biomass.total", "b01", "b02", "b03", "b04",
+"b05", "b06", "b07", "b08", "b09", "b10", "b11", "b12",
+"b13", "b14", "b15", "groundLeaves", "collector",
+"date")
+dat.headers.mono <- c("plot", "type", "sp", "biomass.total",
+"weightTotal", "weightBag", "gl", "glTotal", "glBag",
+NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
+"collector",
+"date")
