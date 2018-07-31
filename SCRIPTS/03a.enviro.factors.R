589da74766f6e4164294ec096a21ac20b11f827a
Ashley Tuffin <tuffinash@gmail.com>
2018-07-31 16:08:34 +0100

    Environmental factors PCA

--- SCRIPTS/03a.enviro.factors.R
+++ SCRIPTS/03a.enviro.factors.R
@@ -3,19 +3,19 @@
 
 ##PCA of environmental factors
 
-#log.envir = log(all.prairie [c(7,11,12,13,14,15)])
-log.envir = log(all.prairie [c(6,10,11,12,13,14)])
+log.envir = log(all.prairie [,c("AHOR_cm","GSM","LOI","fWAS","pH","EC")])
 log.envir = log.envir[-c(409), ] ##AHOR_cm missing from plot 409
-blocks = all.prairie[3]
+blocks = all.prairie["block"]
 blocks = data.frame(blocks[-409, ]) ##AHOR_cm missing from plot 409
 
 pca.envir = prcomp(log.envir, center = TRUE, scale. = TRUE)
 
 library(devtools)
-install_github("ggbiplot", "vqv")
+install_github("vqv/ggbiplot")
 library(plyr)
 library(dplyr)
 
 gg.pca.envir <- ggbiplot(pca.envir, obs.scale = 1, var.scale = 1, groups = blocks[,1], ellipse = TRUE, circle = TRUE)
 gg.pca.envir = gg.pca.envir + scale_color_discrete(name = 'Block')
-print(gg.pca.envir)
+
+pdf('../OUT/pca.environmental.factors.pdf')
