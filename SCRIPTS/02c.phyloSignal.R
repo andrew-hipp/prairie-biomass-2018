a768b8e2d152a452e3cbe2323d5fce132eb1fdf1
Andrew Hipp <andrew.hipp@gmail.com>
2018-08-01 08:48:16 -0500

    Changing rescale to rescale.phylo

--- SCRIPTS/02c.phyloSignal.R
+++ SCRIPTS/02c.phyloSignal.R
@@ -14,7 +14,7 @@ message('Doing Pagel\'s Lambda')
 prairie.lambda <- list(estimated = fitContinuous(tr.prairie.phylosig,
                                                  all.prairie.ordi[, labels.signal],
                                                  model = 'lambda'),
-                       zero = fitContinuous(rescale(tr.prairie.phylosig, 'lambda', 0),
+                       zero = fitContinuous(rescale.phylo(tr.prairie.phylosig, 'lambda', 0),
                                             all.prairie.ordi[, labels.signal])
                                           )
 prairie.phylosignal <- cbind(lambda = sapply(prairie.lambda$estimated, function(x) x$opt$lambda),
