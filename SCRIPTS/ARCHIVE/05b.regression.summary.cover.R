e07d9214af8b432d8a266bceacaa1b294e01792d
lanescher <37148093+lanescher@users.noreply.github.com>
2019-04-02 22:18:03 -0400

    updated new scripts

--- SCRIPTS/ARCHIVE/05b.regression.summary.cover.R
+++ SCRIPTS/ARCHIVE/05b.regression.summary.cover.R
@@ -1,130 +1,128 @@
 # make regression table for cover
-scaled <- as.data.frame(scale(prairie.bio[,81:123]))
-scaled$Plot.category <- prairie.bio$Plot.category
-scaled$TMT.use <- prairie.bio$TMT.use
-scaled$biomass.all <- scale(prairie.bio$biomass.all)
-scaled$coverTotal <- scale(prairie.bio$coverTotal)
-scaled$dcover <- scale(prairie.bio$dcover)
-scaled$AHOR_cm <- scale(prairie.bio$AHOR_cm)
-scaled$VOL <- scale(prairie.bio$VOL)
-scaled$DensVol <- scale(prairie.bio$NDVI * prairie.bio$VOL)
+
+# make scaled dataframe
+scaled <- as.data.frame(all.prairie$plot)
+scaled$Plot.category <- all.prairie$Plot.category
+scaled$TMT.use <- all.prairie$TMT.use
+scaled$biomass.all <- scale(all.prairie$biomass.all)
+scaled$pNDVIvalues <- scale(all.prairie$pNDVIvalues)
+scaled$pGNDVIvalues <- scale(all.prairie$pGNDVIvalues)
+scaled$pGDVI2values <- scale(all.prairie$pGDVI2values)
+scaled$ndvi.avg.threshold <- scale(all.prairie$ndvi.avg.threshold)
+scaled$gndvi.avg.threshold <- scale(all.prairie$gndvi.avg.threshold)
+scaled$gdvi2.avg.threshold <- scale(all.prairie$gdvi2.avg.threshold)
+scaled$ndvi.avg.threshold.noflowers <- scale(all.prairie$ndvi.avg.threshold.noflowers)
+scaled$gndvi.avg.threshold.noflowers <- scale(all.prairie$gndvi.avg.threshold.noflowers)
+scaled$gdvi2.avg.threshold.noflowers <- scale(all.prairie$gdvi2.avg.threshold.noflowers)
+scaled$coverTotal <- scale(all.prairie$coverTotal)
+scaled$dcover <- scale(all.prairie$dcover)
+scaled$AHOR_cm <- scale(all.prairie$AHOR_cm)
+
+use.prairie <- scaled[which(scaled$Plot.category == "Monoculture" | 
+                              scaled$Plot.category == "Treatment"),]
+
+response <- "dcover"
+
+# 1 predictor, VI
+pred1 <- c("pNDVIvalues", "pGNDVIvalues", "pGDVI2values")
 
 
-use.prairie <- scaled
-
-groups <- list(scaled, 
-               scaled[which(scaled$Plot.category == "Monoculture"),],
-               scaled[which(scaled$Plot.category == "Treatment"),])
-namesL <- c("all", "mono", "tmt")
-coverType <- c("dcover", "coverTotal")
+t1 <- as.data.frame(pred1)
 
 
-for (k in 1:2) {
-  response <- coverType[k]
-  
-  for (j in 1:3) {
-    use.prairie <- groups[[j]]
-    
-    
-    # 1 predictor
-    pred1 <- colnames(scaled)[c(1:7)]
-    
-    t1 <- as.data.frame(pred1)
-    
-    # make loop to fill in 1 predictor
-    for (i in 1:length(pred1)) {
-      coef <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred1[i]]]))$coefficients[2,1]
-      pval <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred1[i]]]))$coefficients[2,4]
-      t1[i,2] <- paste(format(round(coef, 2), nsmall = 2), ", p = ", format(round(pval, 4), nsmall = 4), sep = "")
-      t1[i,3] <- "-"
-      t1[i,4] <- format(round(summary(lm(use.prairie[[response]] ~ use.prairie[[pred1[i]]]))$r.squared, 3), nsmall = 3)
-      t1[i,5] <- format(round(AIC(lm(use.prairie[[response]] ~ use.prairie[[pred1[i]]])), 2), nsmall = 2)
-    }
-    
-    t1 <- t1[order(t1$V5),]
-    t1$V5 <- as.numeric(t1$V5)
-    t1$deltaAIC <- t1$V5 - t1$V5[1]
-    
-    colnames(t1) <- c("VI used", "VI", "volume", "R2", "AIC", "deltaAIC")
-    
-    
-    # 1 predictor, vol
-    pred2 <- c("VOL")
-    
-    t2 <- as.data.frame(pred2)
-    
-    # make loop to fill in 1 predictor
-    for (i in 1:length(pred2)) {
-      coef <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred2[i]]]))$coefficients[2,1]
-      pval <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred2[i]]]))$coefficients[2,4]
-      t2[i,2] <- "-"
-      t2[i,3] <- paste(format(round(coef, 2), nsmall = 2), ", p = ", format(round(pval, 4), nsmall = 4), sep = "")
-      t2[i,4] <- format(round(summary(lm(use.prairie[[response]] ~ use.prairie[[pred2[i]]]))$r.squared, 3), nsmall = 3)
-      t2[i,5] <- format(round(AIC(lm(use.prairie[[response]] ~ use.prairie[[pred2[i]]])), 2), nsmall = 2)
-      i <- i + 1
-    }
-    
-    t2 <- t2[order(t2$V5),]
-    t2$V5 <- as.numeric(t2$V5)
-    t2$deltaAIC <- t2$V5 - t2$V5[1]
-    
-    colnames(t2) <- c("VI used", "VI", "volume", "R2", "AIC", "deltaAIC")
-    t2$`VI used` <- "-"
-    
-    # 2 predictors
-    pred2.1 <- colnames(scaled)[c(1:7)]
-    
-    pred2.2 <- rep(c("VOL"), times = 7)
-    
-    t3 <- as.data.frame(pred2.1)
-    
-    for (i in 1:length(pred2.1)) {
-      coef <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred2.1[i]]] + use.prairie[[pred2.2[i]]]))$coefficients[2,1]
-      pval <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred2.1[i]]] + use.prairie[[pred2.2[i]]]))$coefficients[2,4]
-      coef2 <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred2.1[i]]] + use.prairie[[pred2.2[i]]]))$coefficients[3,1]
-      pval2 <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred2.1[i]]] + use.prairie[[pred2.2[i]]]))$coefficients[3,4]
-      t3[i,2] <- paste(format(round(coef, 2), nsmall = 2), ", p = ", format(round(pval, 4), nsmall = 4), sep = "")
-      t3[i,3] <- paste(format(round(coef2, 2), nsmall = 2), ", p = ", format(round(pval2, 4), nsmall = 4), sep = "")
-      t3[i,4] <- format(round(summary(lm(use.prairie[[response]] ~ use.prairie[[pred2.1[i]]] + use.prairie[[pred2.2[i]]]))$r.squared, 3), nsmall = 3)
-      t3[i,5] <- format(round(AIC(lm(use.prairie[[response]] ~ use.prairie[[pred2.1[i]]] + use.prairie[[pred2.2[i]]])), 2), nsmall = 2)
-    }
-    
-    t3 <- t3[order(t3$V5),]
-    t3$V5 <- as.numeric(t3$V5)
-    t3$deltaAIC <- t3$V5 - t3$V5[1]
-    
-    colnames(t3) <- c("VI used", "VI", "volume", "R2", "AIC", "deltaAIC")
-    
-    # merge all tables
-    full <- rbind(t1, t2)
-    full <- rbind(full, t3)
-    
-    
-    # re-sort
-    full <- full[order(full$AIC),]
-    full$AIC <- as.numeric(full$AIC)
-    full$deltaAIC <- full$AIC - full$AIC[1]
-    
-    
-    # format a few things
-    full$VI <- sub("p = 0.0000", "p < 0.0001", full$VI)
-    full$"volume" <- sub("p = 0.0000", "p < 0.0001", full$"volume")
-    
-    assign(paste0(x = "full-", namesL[j], coverType[k]), value = full)
-    
-    # save as csv
-    write.csv(full, paste0("../OUT/TABLE.cover.regression.", namesL[j], ".", coverType[k],".csv"))
-    
-  }
-  
+i <- 1
+# make loop to fill in 1 predictor
+for (i in 1:length(pred1)) {
+  coef <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred1[i]]]))$coefficients[2,1]
+  pval <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred1[i]]]))$coefficients[2,4]
+  t1[i,2] <- paste(format(round(coef, 2), nsmall = 2), ", p = ", format(round(pval, 4), nsmall = 4), sep = "")
+  t1[i,3] <- "-"
+  t1[i,4] <- format(round(summary(lm(use.prairie[[response]] ~ use.prairie[[pred1[i]]]))$r.squared, 3), nsmall = 3)
+  t1[i,5] <- format(round(AIC(lm(use.prairie[[response]] ~ use.prairie[[pred1[i]]])), 2), nsmall = 2)
+  i <- i + 1
 }
 
+t1 <- t1[order(t1$V5),]
+t1$V5 <- as.numeric(t1$V5)
+t1$deltaAIC <- t1$V5 - t1$V5[1]
+
+colnames(t1) <- c("VI used", "VI", "A-horizon", "R2", "AIC", "deltaAIC")
 
 
 
+# 1 predictor, ahor
+pred2 <- c("AHOR_cm")
+
+t2 <- as.data.frame(pred2)
+
+i <- 1
+# make loop to fill in 1 predictor
+for (i in 1:length(pred2)) {
+  coef <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred2[i]]]))$coefficients[2,1]
+  pval <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred2[i]]]))$coefficients[2,4]
+  t2[i,2] <- "-"
+  t2[i,3] <- paste(format(round(coef, 2), nsmall = 2), ", p = ", format(round(pval, 4), nsmall = 4), sep = "")
+  t2[i,4] <- format(round(summary(lm(use.prairie[[response]] ~ use.prairie[[pred2[i]]]))$r.squared, 3), nsmall = 3)
+  t2[i,5] <- format(round(AIC(lm(use.prairie[[response]] ~ use.prairie[[pred2[i]]])), 2), nsmall = 2)
+  i <- i + 1
+}
+
+t2 <- t2[order(t2$V5),]
+t2$V5 <- as.numeric(t2$V5)
+t2$deltaAIC <- t2$V5 - t2$V5[1]
+
+colnames(t2) <- c("VI used", "VI", "A-horizon", "R2", "AIC", "deltaAIC")
+t2$`VI used` <- "-"
+
+# 2 predictors
+
+pred2.1 <- c("pNDVIvalues", "pGNDVIvalues", "pGDVI2values")
+
+pred2.2 <- c("AHOR_cm", "AHOR_cm", "AHOR_cm")
+
+t3 <- as.data.frame(pred2.1)
+
+i <- 1
+for (i in 1:length(pred2.1)) {
+  coef <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred2.1[i]]] + use.prairie[[pred2.2[i]]]))$coefficients[2,1]
+  pval <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred2.1[i]]] + use.prairie[[pred2.2[i]]]))$coefficients[2,4]
+  coef2 <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred2.1[i]]] + use.prairie[[pred2.2[i]]]))$coefficients[3,1]
+  pval2 <- summary(lm(use.prairie[[response]] ~ use.prairie[[pred2.1[i]]] + use.prairie[[pred2.2[i]]]))$coefficients[3,4]
+  t3[i,2] <- paste(format(round(coef, 2), nsmall = 2), ", p = ", format(round(pval, 4), nsmall = 4), sep = "")
+  t3[i,3] <- paste(format(round(coef2, 2), nsmall = 2), ", p = ", format(round(pval2, 4), nsmall = 4), sep = "")
+  t3[i,4] <- format(round(summary(lm(use.prairie[[response]] ~ use.prairie[[pred2.1[i]]] + use.prairie[[pred2.2[i]]]))$r.squared, 3), nsmall = 3)
+  t3[i,5] <- format(round(AIC(lm(use.prairie[[response]] ~ use.prairie[[pred2.1[i]]] + use.prairie[[pred2.2[i]]])), 2), nsmall = 2)
+  i <- i + 1
+}
+
+t3 <- t3[order(t3$V5),]
+t3$V5 <- as.numeric(t3$V5)
+t3$deltaAIC <- t3$V5 - t3$V5[1]
+
+colnames(t3) <- c("VI used", "VI", "A-horizon", "R2", "AIC", "deltaAIC")
+
+# merge all tables
+full <- rbind(t1, t2)
+full <- rbind(full, t3)
 
 
+# re-sort
+full <- full[order(full$AIC),]
+full$AIC <- as.numeric(full$AIC)
+full$deltaAIC <- full$AIC - full$AIC[1]
 
+
+# format a few things
+full$`VI used` <- sub("pNDVIvalues", "NDVI", full$`VI used`)
+full$`VI used` <- sub("pGNDVIvalues", "GNDVI", full$`VI used`)
+full$`VI used` <- sub("pGDVI2values", "GDVI2", full$`VI used`)
+
+
+full$VI <- sub("p = 0.0000", "p < 0.0001", full$VI)
+full$"A-horizon" <- sub("p = 0.0000", "p < 0.0001", full$"A-horizon")
+
+# save as csv
+write.csv(full, "../OUT/TABLE.coverdrone.regression.csv")
 
 
 #### Do it again for cover from the ground
--- SCRIPTS/ARCHIVE/06.making.graphs.R
+++ SCRIPTS/ARCHIVE/06.making.graphs.R
@@ -4,19 +4,21 @@ library(ggplot2)
 library(colortools)
 library(ggpubr)
 
-# use for biomass analyses
-prairie.use.biomass <- prairie.bio
 
-# uses for NDVI and cover analyses
-prairie.use.other <- prairie
+prairie.use.biomass <- all.prairie[which(all.prairie$Plot.category == "Monoculture" |
+                                   all.prairie$TMT.use == 1),]
+
+prairie.use.other <- all.prairie[which(all.prairie$Plot.category == "Monoculture" |
+                                         all.prairie$Plot.category == "Treatment"),]
 
 
 ################ regression
 
 
+# combined
 # NDVI biomass regression
 NBR <- ggplot(data = prairie.use.biomass,
-                 aes(x = prairie.use.biomass$NDVI, y = prairie.use.biomass$biomass.all)) + #chnaged from pNDVIvalues
+       aes(x = prairie.use.biomass$pNDVIvalues, y = prairie.use.biomass$biomass.all)) +
   geom_point(aes(color = factor(prairie.use.biomass$Plot.category))) +
   geom_smooth(method = "lm", aes(color = factor(prairie.use.biomass$Plot.category))) +
   labs(x = "NDVI", y = "biomass") +
@@ -26,23 +28,12 @@ NBR <- ggplot(data = prairie.use.biomass,
                       guide = FALSE) +
   theme_classic()
 
-NBR.CI <- ggplot(data = prairie.use.biomass,
-       aes(x = prairie.use.biomass$NDVI.CI, y = prairie.use.biomass$biomass.all)) + #chnaged from pNDVIvalues
-  geom_point(aes(color = factor(prairie.use.biomass$Plot.category))) +
-  geom_smooth(method = "lm", aes(color = factor(prairie.use.biomass$Plot.category))) +
-  labs(x = "NDVI (excluding weeds)", y = "biomass (excluding weeds)") +
-  scale_colour_manual(values = c("goldenrod2", "cornflowerblue"),
-                      name = "Plot type",
-                      breaks = c("Monoculture", "Treatment"),
-                      guide = FALSE) +
-  theme_classic()
-
 # NDVI ground cover regression
-NGCR.CI <- ggplot(data = prairie.use.other,
-              aes(x = prairie.use.other$NDVI.CI, y = prairie.use.other$coverTotal)) + #chnaged from pNDVIvalues
+NGCR <- ggplot(data = prairie.use.other,
+              aes(x = prairie.use.other$pNDVIvalues, y = prairie.use.other$coverTotal)) +
   geom_point(aes(color = factor(prairie.use.other$Plot.category))) +
   geom_smooth(method = "lm", aes(color = factor(prairie.use.other$Plot.category))) +
-  labs(x = "NDVI (excluding weeds)", y = "percent cover (excluding weeds)") +
+  labs(x = "NDVI", y = "percent cover, ground") +
   scale_colour_manual(values = c("goldenrod2", "cornflowerblue"),
                       name = "Plot type",
                       breaks = c("Monoculture", "Treatment")) +
@@ -51,10 +42,10 @@ NGCR.CI <- ggplot(data = prairie.use.other,
 
 # NDVI drone cover regression
 NDCR <- ggplot(data = prairie.use.other,
-              aes(x = prairie.use.other$NDVI, y = prairie.use.other$dcover)) + #chnaged from pNDVIvalues
+              aes(x = prairie.use.other$pNDVIvalues, y = prairie.use.other$dcover)) +
   geom_point(aes(color = factor(prairie.use.other$Plot.category))) +
   geom_smooth(method = "lm", aes(color = factor(prairie.use.other$Plot.category))) +
-  labs(x = "NDVI (including weeds)", y = "percent cover (including weeds)") +
+  labs(x = "NDVI", y = "percent cover, drone") +
   scale_colour_manual(values = c("goldenrod2", "cornflowerblue"),
                       name = "Plot type",
                       breaks = c("Monoculture", "Treatment")) +
@@ -66,21 +57,38 @@ BGCR <- ggplot(data = prairie.use.biomass,
                aes(x = prairie.use.biomass$coverTotal, y = prairie.use.biomass$biomass.all)) +
   geom_point(aes(color = factor(prairie.use.biomass$Plot.category))) +
   geom_smooth(method = "lm", aes(color = factor(prairie.use.biomass$Plot.category))) +
-  labs(x = "percent cover (excluding weeds)", y = "biomass (excluding weeds)") +
+  labs(x = "percent cover, ground", y = "biomass") +
   scale_colour_manual(values = c("goldenrod2", "cornflowerblue"),
                       name = "Plot type",
                       breaks = c("Monoculture", "Treatment")) +
   theme_classic()
 
 
+
 jpeg("../OUT/FIGURE.regressions.jpg", width = 900, height = 900)
-ggarrange(NBR.CI, BGCR, NGCR.CI, NDCR, labels = c("A", "B", "C", "D"), nrow = 2, ncol = 2,
+ggarrange(NBR, BGCR, NGCR, NDCR, labels = c("A", "B", "C", "D"), nrow = 2, ncol = 2,
           common.legend = TRUE, legend = "bottom",
           label.x = 1, label.y = 1, align = "hv")
 dev.off()
 
 
 ############# boxplots
+
+darken <- function(color, factor=1.4){
+  col <- col2rgb(color)
+  col <- col/factor
+  col <- rgb(t(col), maxColorValue=255)
+  col
+}
+
+lighten <- function(color, factor=1.4){
+  col <- col2rgb(color)
+  col <- col*factor
+  col <- rgb(t(as.matrix(apply(col, 1, function(x) if (x > 255) 255 else x))), maxColorValue=255)
+  col
+}
+
+
 
 # biomass
 MB <- ggplot(data = prairie.use.biomass,
@@ -91,6 +99,21 @@ MB <- ggplot(data = prairie.use.biomass,
   labs(x = "plot type", y = "biomass") +
   ylim(c(0, 4000))
 
+TB <- ggplot(data = prairie.use.biomass,
+       aes(x = prairie.use.biomass$trait.div, y = prairie.use.biomass$biomass.all)) +
+  geom_boxplot(fill = c(lighten("cornflowerblue"), darken("cornflowerblue"))) +
+  scale_x_discrete(limits = c("L", "H"), labels = c("Low", "High")) +
+  theme_classic() +
+  labs(x = "trait diversity", y = "biomass") +
+  ylim(c(0, 4000))
+
+PB <- ggplot(data = prairie.use.biomass,
+       aes(x = prairie.use.biomass$phy.div, y = prairie.use.biomass$biomass.all)) +
+  geom_boxplot(fill = c(lighten("cornflowerblue"), "cornflowerblue", darken("cornflowerblue"))) +
+  scale_x_discrete(limits = c("L", "M", "H"), labels = c("Low", "Medium", "High")) +
+  theme_classic() +
+  labs(x = "phylogenetic diversity", y = "biomass") +
+  ylim(c(0, 4000))
 
 BB <- ggplot(data = prairie.use.biomass,
        aes(x = prairie.use.biomass$block, y = prairie.use.biomass$biomass.all)) +
@@ -101,18 +124,33 @@ BB <- ggplot(data = prairie.use.biomass,
   ylim(c(0, 4000))
 
 # NDVI
-#Removed 45 rows containing non-finite values (stat_boxplot). 
+
+TN <- ggplot(data = prairie.use.other,
+       aes(x = prairie.use.other$trait.div, y = prairie.use.other$pNDVIvalues)) +
+  geom_boxplot(fill = c(lighten("cornflowerblue"), darken("cornflowerblue"))) +
+  scale_x_discrete(limits = c("L", "H"), labels = c("Low", "High")) +
+  theme_classic() +
+  labs(x = "trait diversity", y = "NDVI") +
+  ylim(c(0.3, 0.9))
+
+PN <- ggplot(data = prairie.use.other,
+       aes(x = prairie.use.other$phy.div, y = prairie.use.other$pNDVIvalues)) +
+  geom_boxplot(fill = c(lighten("cornflowerblue"), "cornflowerblue", darken("cornflowerblue"))) +
+  scale_x_discrete(limits = c("L", "M", "H"), labels = c("Low", "Medium", "High")) +
+  theme_classic() +
+  labs(x = "phylogenetic diversity", y = "NDVI") +
+  ylim(c(0.3, 0.9))
+
 BN <- ggplot(data = prairie.use.other,
-             aes(x = prairie.use.other$block, y = prairie.use.other$NDVI.CI)) + #chnaged from pNDVIvalues
+             aes(x = prairie.use.other$block, y = prairie.use.other$pNDVIvalues)) +
   geom_boxplot(fill = "darkolivegreen4") +
   scale_x_discrete(limits = c("A", "B", "C", "D", "E", "F")) +
   theme_classic() +
   labs(x = "block", y = "NDVI") +
   ylim(c(0.3, 0.9))
 
-#Removed 45 rows containing non-finite values (stat_boxplot).
 MN <- ggplot(data = prairie.use.other,
-       aes(x = prairie.use.other$Plot.category, y = prairie.use.other$NDVI.CI)) + #chnaged from pNDVIvalues
+       aes(x = prairie.use.other$Plot.category, y = prairie.use.other$pNDVIvalues)) +
   geom_boxplot(fill = c("goldenrod2", "cornflowerblue")) +
   scale_x_discrete(limits = c("Monoculture", "Treatment"), labels = c("Monoculture", "Treatment")) +
   theme_classic() +
@@ -120,7 +158,22 @@ MN <- ggplot(data = prairie.use.other,
   ylim(c(0.3, 0.9))
 
 # Cover
-# Removed 5 rows containing non-finite values (stat_boxplot).
+TC <- ggplot(data = prairie.use.other,
+             aes(x = prairie.use.other$trait.div, y = prairie.use.other$coverTotal)) +
+  geom_boxplot(fill = c(lighten("cornflowerblue"), darken("cornflowerblue"))) +
+  scale_x_discrete(limits = c("L", "H"), labels = c("Low", "High")) +
+  theme_classic() +
+  labs(x = "trait diversity", y = "cover") +
+  ylim(c(0, 100))
+
+PC <- ggplot(data = prairie.use.other,
+             aes(x = prairie.use.other$phy.div, y = prairie.use.other$coverTotal)) +
+  geom_boxplot(fill = c(lighten("cornflowerblue"), "cornflowerblue", darken("cornflowerblue"))) +
+  scale_x_discrete(limits = c("L", "M", "H"), labels = c("Low", "Medium", "High")) +
+  theme_classic() +
+  labs(x = "phylogenetic diversity", y = "cover") +
+  ylim(c(0, 100))
+
 BC <- ggplot(data = prairie.use.other,
              aes(x = prairie.use.other$block, y = prairie.use.other$coverTotal)) +
   geom_boxplot(fill = "darkolivegreen4") +
@@ -129,7 +182,6 @@ BC <- ggplot(data = prairie.use.other,
   labs(x = "block", y = "cover") +
   ylim(c(0, 100))
 
-#Removed 5 rows containing non-finite values (stat_boxplot).
 MC <- ggplot(data = prairie.use.other,
              aes(x = prairie.use.other$Plot.category, y = prairie.use.other$coverTotal)) +
   geom_boxplot(fill = c("goldenrod2", "cornflowerblue")) +
@@ -139,13 +191,12 @@ MC <- ggplot(data = prairie.use.other,
   ylim(c(0, 100))
 
 
-# format and save figure
-jpeg("../OUT/FIGURE.boxplots.jpg", width = 510, height = 480)
-ggarrange(MB, BB,
-          MC, BC,
-          MN, BN,
-          labels = c("A", "B", "C", "D", "E", "F"),
-          ncol = 2, nrow = 3,
+jpeg("../OUT/FIGURE.boxplots.jpg", width = 1100, height = 480)
+ggarrange(MB, TB, PB, BB,
+          MC, TC, PC, BC,
+          MN, TN, PN, BN,
+          labels = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"),
+          ncol = 4, nrow = 3,
           align = "hv",
           label.x = 0, label.y = 1)
 dev.off()
